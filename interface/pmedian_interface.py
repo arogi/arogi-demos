b70741c35dbd3877e97a99f706aa6e679b9044b2
glennon <glennon@gmail.com>
2016-05-05 17:48:34 -0700

    capitalization correction

--- interface/pmedian_interface.py
+++ interface/pmedian_interface.py
@@ -1,308 +1,221 @@
-# Copyright 2015-2016 Arogi Inc
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# Authors: Antonio Medrano and Matt Niblett
+#!/usr/bin/python
 
-
-"""A p-Median example that imports a JSON file problem definition and solves using
-   a standard IP formulation."""
-
-import sys
+import cgi, cgitb
 import json
 import GISOps
-import time
 import numpy as np
 from scipy.spatial.distance import cdist
 from ortools.linear_solver import pywraplp
 
-p = -1
 
-printModel = False
+def main():
+    ReadJSONandSolve()
 
 
+def readJSONandSolve(jsonStrObj,p):
+    p = read_problem(receivedMarkerData)
+    RunALLMCLPexampleCppStyleAPI(p)
+
+
+def RunAllPMedianExampleCppStyleAPI(p):
+    if hasattr(pywraplp.Solver, 'CBC_MIXED_INTEGER_PROGRAMMING'):
+        RunPMedianExampleCppStyleAPI(pywraplp.Solver.CBC_MIXED_INTEGER_PROGRAMMING, p)
+        
+        
 def RunPMedianExampleCppStyleAPI(optimization_problem_type, p):
-  """Example of simple p-median program with the C++ style API."""
-  solver = pywraplp.Solver('RunIntegerExampleCppStyleAPI',
+    """solve a standard formulation p-median problem"""
+    solver = pywraplp.Solver('RunIntegerExampleCppStyleAPI',
                            optimization_problem_type)
-  infinity = solver.infinity()
-  start_time = time.time()
+
+    # 1 if demand i is served by facility j
+    X = [[None for j in range(numFacilities)] for i in range(numDemands)]
+    # 1 if facility at site j is located
+    Y = [None]*numFacilities
+
+    PreComputeDistances()
+
+    BuildModel(solver, X, Y, p)
+    SolveModel(solver, X, Y, p)
+
+    generateGEOJSON(X, Y, p)
+    displaySolution(solver, Y, p)
+    return 1
+
+
+def read_problem(file):
+    global numFeatures
+    global numFacilities
+    global facilityIDs
+    global forcedFacilities
+    global numForced
+    global numDemands
+    global demandIDs
+    global demandPop
+    global js
   
-  #declare a couple variables
-  name = ''
-  global d
+    try:
+        js = json.loads(file)
+    except IOError:
+      print "unable to read file"
+
+    numFeatures = len(js['features'])
+
+    # if the geoJSON includes a p value, use this rather than any input arguments
+    try:
+        p = js['properties']['pValue']
+    except IOError:
+        print "geoJSON has no pValue"
+
+    facilityIDs = []
+    demandIDs = []
+    forcedFacilities = []
   
-  # 1 if demand i is served by facility j
-  X = [[-1 for j in range(numFacilities)] for i in range(numDemands)]
-  # 1 if facility at site j is located
-  Y = [-1]*numFacilities
+    # rowID holds the index of each feature in the JSON object
+    rowID = 0
+
+    # typeFD = Field Codes Represent:
+    #  1 = demand only
+    #  2 = potential facility only
+    #  3 = both demand and potential facility
+    for element in js['features']:
+        if element['properties']['typeFD']==3: # Both Facility/Demand
+            facilityIDs.append(rowID)
+            demandIDs.append(rowID)
+        elif element['properties']['typeFD']==2: # Facility Site Only
+            facilityIDs.append(rowID)
+        elif element['properties']['typeFD']==1: # Demand Point Only
+            demandIDs.append(rowID)
+        if element['properties']['forcedLocation'] == 1:
+            forcedFacilities.append(rowID)
+        rowID += 1
   
-  A = [demandArray[i][0:2] for i in range(numDemands)]    
-  B = [facilityArray[j][0:2] for j in range(numFacilities)]
+    numFacilities = len(facilityIDs)
+    numDemands = len(demandIDs)
+    numForced = len(forcedFacilities)
+
+    demandPop = [js['features'][i]['properties']['pop'] for i in demandIDs]
   
-  d = cdist(A, B,'euclidean')
-  
-  # DECLARE CONSTRAINTS
-  # declare facility location constraints
-  c1 = [None]*numDemands
-  # declare allocation constraints
-  c2 = [None]*(numDemands*numFacilities)
-  # explicitly declare and initialize the p-facility constraint
-  c3 = solver.Constraint(p,p)  # equality constraint equal to p
-  
-  # declare the objective
-  objective = solver.Objective()
-  objective.SetMinimization()
-  
-  # Generate the objective function and constraints
-  for j in range(numFacilities):
-    # initialize the Y site location variables
-    name = "Y,%d" % (j+1)
-    Y[j] = solver.BoolVar(name)
-    # set coefficients of the Y variables in constraint 3
-    c3.SetCoefficient(Y[j], 1)
-  
-  for i in range(numDemands):
+    # check if valid for the given p
+    try:
+        if numForced > p:
+            raise DataError('numForcedGreaterThanP')
+    except DataError:
+        print 'number of forced facilities is greater than p'
+        raise
+
+    print 'Finished Reading the Data!'
+    return p
     
-    # initialize c1 equality constraints = 1
-    c1[i] = solver.Constraint(1, 1)
-    
+
+def BuildModel(solver, X, Y, p):
+  
+    infinity = solver.infinity()
+
+    # DECLARE CONSTRAINTS
+    # declare facility location constraints
+    c1 = [None]*numDemands
+    # declare allocation constraints
+    c2 = [None]*(numDemands*numFacilities)
+    # explicitly declare and initialize the p-facility constraint
+    c3 = solver.Constraint(p,p)  # equality constraint equal to p
+
+    # declare the objective
+    objective = solver.Objective()
+    objective.SetMinimization()
+
+    # Generate the objective function and constraints
     for j in range(numFacilities):
-      
-      # initialize the X assignment variables and add them to the objective function
-      name = 'X,%d,%d' % (i+1,j+1)
-      X[i][j] = solver.BoolVar(name)
-      objective.SetCoefficient(X[i][j], demandArray[i][2]*d[i,j])
-      
-      # set the variable coefficients of the sum(Xij) = 1 for each i
-      c1[i].SetCoefficient(X[i][j], 1)
-      
-      # Yj - Xij >= 0 <--- canonical form of the assignment constraint
-      c2[i*numFacilities+j] = solver.Constraint(0, infinity) # c2 rhs
-      c2[i*numFacilities+j].SetCoefficient(X[i][j], -1)
-      c2[i*numFacilities+j].SetCoefficient(Y[j], 1)
+        # initialize the Y site location variables
+        Y[j] = solver.BoolVar('Y_%d' % j)
+        # set coefficients of the Y variables in constraint 3
+        c3.SetCoefficient(Y[j], 1)
 
-  end_time = time.time()
-  print("total time to set up problem: ", end_time - start_time)
+    for i in range(numDemands):
+
+        # initialize c1 equality constraints = 1
+        c1[i] = solver.Constraint(1, 1)
+
+        for j in range(numFacilities):
   
-  if printModel:
-    model = solver.ExportModelAsLpFormat(False)
-    print(model)
-  
-  SolveAndPrint(solver, X, Y, p)
+            # initialize the X assignment variables and add them to the objective function
+            X[i][j] = solver.BoolVar('X_%d,%d' % (i,j))
+            objective.SetCoefficient(X[i][j], demandPop[i]*d[i,j])
+
+            # set the variable coefficients of the sum(Xij) = 1 for each i
+            c1[i].SetCoefficient(X[i][j], 1)
+
+            # Yj - Xij >= 0 <--- canonical form of the assignment constraint
+            c2[i*numFacilities+j] = solver.Constraint(0, infinity) # c2 rhs
+            c2[i*numFacilities+j].SetCoefficient(X[i][j], -1)
+            c2[i*numFacilities+j].SetCoefficient(Y[j], 1)  
+
+    print 'Number of variables = %d' % solver.NumVariables()
+    print 'Number of constraints = %d' % solver.NumConstraints()
+    print
+    return 1
 
 
-def SolveAndPrint(solver, X, Y, p):
-  """Solve the problem and print the solution."""
-  print 'Number of variables = %d' % solver.NumVariables()
-  print 'Number of constraints = %d' % solver.NumConstraints()
+def PreComputeDistances():
+    
+    #declare a couple variables
+    global dSort
+    global d
+    
+    # Convert Coordinates from Lat/Long to CONUS EqD Projection
+    xyPointArray = GISOps.GetCONUSeqDprojCoords(js)
+    
+    A = [xyPointArray[i][:] for i in demandIDs]
+    B = [xyPointArray[j][:] for j in facilityIDs]
+    
+    d = cdist(A, B,'euclidean')
+    return 1
 
-  result_status = solver.Solve()
 
-  # The problem has an optimal solution.
-  assert result_status == pywraplp.Solver.OPTIMAL, "The problem does not have an optimal solution!"
+def SolveModel(solver, X, Y, p):
+    """Solve the problem"""
+    result_status = solver.Solve()
 
-  # The solution looks legit (when using solvers others than
-  # GLOP_LINEAR_PROGRAMMING, verifying the solution is highly recommended!).
-  assert solver.VerifySolution(1e-7, True)
+    # The problem has an optimal solution.
+    assert result_status == pywraplp.Solver.OPTIMAL, "The problem does not have an optimal solution!"
 
-  print 'Problem solved in %f milliseconds' % solver.wall_time()
-
-  # The objective value of the solution.
-  print 'Optimal objective value = %f' % solver.Objective().Value()
-
-  # print the selected sites
-  print
-  count = 0
-  for j in facilityIDs:
-    if (Y[count].SolutionValue() == True):
-      print "Facility selected %d" % (js['features'][j]['properties']['pointID'])
-    count += 1
-      
-  generateGEOJSON(X, Y, p)
+    # The solution looks legit (when using solvers others than
+    # GLOP_LINEAR_PROGRAMMING, verifying the solution is highly recommended!).
+    assert solver.VerifySolution(1e-7, True)
+    return 1
 
 
 def generateGEOJSON(X, Y, p):
   
-  unweightedObj = 0
-  weightedObj = 0
-  assignment = -1
-  #located = -1
-  
-  for j in range(numFacilities):
-    located = Y[j].SolutionValue()
-    js['features'][facilityIDs[j]]['properties']['facilityLocated'] = located
-    #js['features'][facilityIDs[j]]['properties']['assignedTo'] = facilityIDs[j]
-    
-    # THIS GIVES THE WRONG OUPUT
-    # for i in range(numDemands):
-    #   if (X[i][j].SolutionValue() == True):
-    #     print i,demandIDs[i],facilityIDs[j]
-    #     js['features'][demandIDs[i]]['properties']['assignedTo'] = facilityIDs[j]  
-  
-  for i in range(numDemands):
-      for j in range(numFacilities):
-        if (X[i][j].SolutionValue() == True):
-          #print i,demandIDs[i],facilityIDs[j], demandArray[i][2]*d[i,j]
-          js['features'][demandIDs[i]]['properties']['assignedTo'] = facilityIDs[j]
-  
-  #print demandIDs  
-  writeToGJSFile(js, p)
-        
-  
-def writeToGJSFile(js, p):
-    
-  with open('./data/PMedianResult_s%d_p%d.json' % (numFeatures, p), 'w') as outfile:
-    json.dump(js,outfile)
+    for j in range(numFacilities):
+        located = Y[j].SolutionValue()
+        js['features'][facilityIDs[j]]['properties']['facilityLocated'] = located 
+
+    for i in range(numDemands):
+        for j in range(numFacilities):
+            if (X[i][j].SolutionValue() == True):
+                js['features'][demandIDs[i]]['properties']['assignedTo'] = facilityIDs[j]
+                break
+    writeToGJSFile(js, p)
+    return 1
 
 
-def Announce(solver, api_type):
-  print ('---- Integer programming example with ' + solver + ' (' +
-         api_type + ') -----')
 
-
-def RunAllPMedianExampleCppStyleAPI(p):
-  if hasattr(pywraplp.Solver, 'GLPK_MIXED_INTEGER_PROGRAMMING'):
-    Announce('GLPK', 'C++ style API')
-    RunPMedianExampleCppStyleAPI(pywraplp.Solver.GLPK_MIXED_INTEGER_PROGRAMMING, p)
-  if hasattr(pywraplp.Solver, 'CBC_MIXED_INTEGER_PROGRAMMING'):
-    Announce('CBC', 'C++ style API')
-    RunPMedianExampleCppStyleAPI(pywraplp.Solver.CBC_MIXED_INTEGER_PROGRAMMING, p)
-  if hasattr(pywraplp.Solver, 'SCIP_MIXED_INTEGER_PROGRAMMING'):
-    Announce('SCIP', 'C++ style API')
-    RunPMedianExampleCppStyleAPI(pywraplp.Solver.SCIP_MIXED_INTEGER_PROGRAMMING, p)
-
-#
-# Read a problem instance from a file
-#
-def read_problem(file, p, readType):
-  global numFeatures
-  global numFacilities
-  global facilityIDs
-  global facilityArray
-  global numForced
-  global numDemands
-  global demandIDs
-  global demandArray
-  global js
-  global jsonRowDictionary
-  
-  if readType == 1:
-    print 'Reading JSON String Object'
-    js = json.loads(file)
-  elif readType == 2:
-    print 'readFile({0})'.format(file)
-    with open(file,"r") as f:
-      js = json.load(f)
-  else:
-    print "READ TYPE ERROR!!"
-
-  numFeatures = len(js['features'])
-
-  # if the geoJSON includes a p value, use this rather than any input arguments
-  try:
-    p = js['properties']['pValue']
-  except IOError:
-    print "geoJSON has no pValue"
-  
-  xyPointArray = [[None for k in range(2)] for j in range(numFeatures)]
-  xyPointArray = GISOps.GetCONUSeqDprojCoords(js) # Get the Distance Coordinates in CONUS EqD Projection
-
-  facilityIDs = []
-  demandIDs = []
-  
-  # rowID holds the index of each feature in the JSON object
-  rowID = 0
-  
-  # typeFD = Field Codes Represent:
-  #  1 = demand only
-  #  2 = potential facility only
-  #  3 = both demand and potential facility
-  for element in js['features']:      
-            
-      if element['properties']['typeFD']==3: # Both Facility/Demand
-          facilityIDs.append(rowID)
-          demandIDs.append(rowID)
-      elif element['properties']['typeFD']==2: # Facility Site Only
-          facilityIDs.append(rowID)
-      elif element['properties']['typeFD']==1: # Demand Point Only
-          demandIDs.append(rowID)
-      rowID += 1
-    
-  numFacilities = len(facilityIDs)
-  numDemands = len(demandIDs)
-
-  print("Number of Facilities: {0}".format(numFacilities))
-  print('Number of Demands: {0}'.format(numDemands))
-  
-  facilityArray = [[None for k in range(3)] for j in range(numFacilities)]
-  demandArray = [[None for k in range(3)] for j in range(numDemands)]
-  
-  i = 0
-  j = 0
-  k = 0
-  for line in js['features']:
-    if line['properties']['typeFD']>=2:
-      facilityArray[i][0] = xyPointArray[k][0]
-      facilityArray[i][1] = xyPointArray[k][1]
-      facilityArray[i][2] = line['properties']['forcedLocation']
-      i += 1
-    if line['properties']['typeFD'] % 2 == 1:
-      demandArray[j][0] = xyPointArray[k][0]
-      demandArray[j][1] = xyPointArray[k][1]
-      demandArray[j][2] = line['properties']['pop']
-      j += 1
-    k += 1
-  
-  numForced = sum(zip(*facilityArray)[2])
-  # check if valid for the given p
-  try:
-    if numForced > p:
-      raise DataError('numForcedGreaterThanP')
-  except DataError:
-    print 'number of forced facilities is greater than p'
-    raise
-  
-  print 'Finished Reading the Data!'
-  return p
 
     
-def readJSONstrObjANDsolve(jsonStrObj,p):
-  readType = 1
-  
-  p = read_problem(jsonStrObj, p, readType)
-  main(p)
-  return js
+###########################################################
+##################### The main controller code starts here.
+###########################################################
 
-def main(p):
-  print "Setting up and solving problem!"
-  RunAllPMedianExampleCppStyleAPI(p)
-  if js != None:
-    return js
-  else:
-    print 'YOU ARE DUMB! You should be using exception handling!'
-    return None
+# Create instance of FieldStorage and get data
+form = cgi.FieldStorage()
+receivedMarkerData = form.getvalue('useTheseMarkers')
+## convert the received json string into a Python object
+#receivedGeoJson = json.loads(receivedMarkerData)
 
-if __name__ == '__main__':
-  readType = 2
-  
-  if len(sys.argv) > 2:
-    p = float(sys.argv[1])
-    file = sys.argv[2]
-    print "Problem instance from", file
-    p = read_problem(file)
-    main(p)
-  elif len(sys.argv) > 1:
-    p = float(sys.argv[1])
-    main(p)
-  else:
-    main(None)
+# the magic happens here...
+main()
+
+# prepare for output... the GeoJSON should be returned as a string
+transformedMarkerData = json.dumps(js)
+print "Content-type:text/html\r\n\r\n"
+print transformedMarkerData
